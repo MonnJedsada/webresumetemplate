f07787364625e41b89f6f39b1ea0a305b7e9fe5e
MonnJedsada <jedsadavong@gmail.com>
2022-07-19 11:40:31 +0700

    Sample code
    sample code blog

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+$(window).on('scroll', function () {
+    var scrollTop = $(this).scrollTop()
+    var score = '15px'
+    if (scrollTop > 1) {
+        score = '5px 15px'
+    }
+    $('#navbar').css('padding', score)
+});
+
+$(function(){
+    // global variables
+    captchaResized = false;
+    captchaWidth = 304;
+    captchaHeight = 78;
+    captchaWrapper = $('#recaptcha-wrapper');
+    captchaElements = $('#rc-imageselect, .g-recaptcha');
+
+    $(window).on('resize', function() {
+        resizeCaptcha();
+    });
+
+    resizeCaptcha();
+});
+
+function resizeCaptcha() {
+    if (captchaWrapper.width() >= captchaWidth) {
+        if (captchaResized) {
+            captchaElements.css('transform', '').css('-webkit-transform', '').css('-ms-transform', '').css('-o-transform', '').css('transform-origin', '').css('-webkit-transform-origin', '').css('-ms-transform-origin', '').css('-o-transform-origin', '');
+            captchaWrapper.height(captchaHeight);
+            captchaResized = false;
+        }
+    } else {
+        var scale = (1 - (captchaWidth - captchaWrapper.width()) * (0.05/15));
+        captchaElements.css('transform', 'scale('+scale+')').css('-webkit-transform', 'scale('+scale+')').css('-ms-transform', 'scale('+scale+')').css('-o-transform', 'scale('+scale+')').css('transform-origin', '0 0').css('-webkit-transform-origin', '0 0').css('-ms-transform-origin', '0 0').css('-o-transform-origin', '0 0');
+        captchaWrapper.height(captchaHeight * scale);
+        if (captchaResized == false) captchaResized = true;
+    }
+}
+// resizeCaptcha();
+
+$('.to-top').click(function (e){
+    $('html, body').animate({scrollTop: '0px'}, 800)
+})
